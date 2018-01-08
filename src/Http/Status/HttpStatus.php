1074a286b62c19045378b5b71938b458cc286663
kento-oka <kento.oka@kentoka.com>
2018-01-08 22:51:17 +0900

    HTTPstatusの処理を書き換えようかな

--- src/Http/Status/HttpStatus.php
+++ src/Http/Status/HttpStatus.php
@@ -14,14 +14,58 @@
 namespace Fratily\Http\Status;
 
 /**
- * 
+ *
  */
 class HttpStatus extends \Exception{
-    
+
+    const STATUS_PHRASES    = [
+        400 => "Bad Request",
+        401 => "Unauthorized",
+        402 => "Payment Required",
+        403 => "Forbidden",
+        404 => "Not Found",
+        405 => "Method Not Allowed",
+        406 => "Not Acceptable",
+        407 => "Proxy Authentication Required",
+        408 => "Request Timeout",
+        409 => "Conflict",
+        410 => "Gone",
+        411 => "Length Required",
+        412 => "Precondition Failed",
+        413 => "Payload Too Large",
+        414 => "URI Too Long",
+        415 => "Unsupported Media Type",
+        416 => "Requested Range Not Satisfiable",
+        417 => "Expectation Failed",
+        418 => "I'm a teapot",
+        421 => "Misdirected Request",
+        422 => "Unprocessable Entity",
+        423 => "Locked",
+        424 => "Failed Dependency",
+        426 => "Upgrade Required",
+        428 => "Precondition Required",
+        429 => "Too Many Requests",
+        431 => "Request Header Fields Too Large",
+        451 => "Unavailable For Legal Reasons",
+    ];
+
+    const STATUS    = 500;
+
+    private $status;
+
     /**
      * Constructor
      */
-    public function __construct(){
-        parent::__construct(static::MSG ?? "Not Found", static::CODE ?? 404);
+    public function __construct(string $msg = null, int $code = 0){
+
+        parent::__construct($msg, $code);
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+
+    public function getPhrase(){
+        return self::STATUS_PHRASES[$this->status] ?? "";
     }
 }
