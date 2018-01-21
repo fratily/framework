2152d1220524eaf97cffc4bf1b1ad900f9163b87
kento-oka <kento.oka@kentoka.com>
2018-01-21 18:16:43 +0900

    serviceの取得方法を変更

--- src/Application/Controller.php
+++ src/Application/Controller.php
@@ -36,11 +36,11 @@ abstract class Controller{
         $this->container        = $container;
     }
 
-    protected function getService(string $id){
+    protected function __get($id){
         return $this->container->get($id);
     }
 
-    protected function hasService(string $id){
+    protected function __isset($id){
         return $this->container->has($id);
     }
 }
