c5c35f4d575e90d6f2defd8a62089bbb4987dc3e
kento-oka <kento.oka@kentoka.com>
2018-01-21 18:16:17 +0900

    記述を修正

--- src/Application/Configure.php
+++ src/Application/Configure.php
@@ -16,31 +16,31 @@ namespace Fratily\Application;
 use Psr\Container\ContainerInterface;
 
 /**
- * 
- * 
+ *
+ *
  * @property-read   ContainerInterface  $container
  */
 abstract class Configure{
-    
+
     const ALLOW_OVERWRITE   = 1;
-    
+
     private static $config  = [
         1   => []
     ];
-    
+
     /**
      * ノードから指定したキーに一致する子ノードをすべて取得する
-     * 
+     *
      * @param   mixed[] $node
      * @param   string  $key
-     * 
+     *
      * @return  array[]|null
      */
     private static function getChildren(array $node, string $key){
         if(!isset($node[1]) || !is_array($node[1])){
             return null;
         }
-        
+
         if($key === "*"){
             if(empty($node[1])){
                 return null;
@@ -51,37 +51,37 @@ abstract class Configure{
             if(!isset($node[1][$key])){
                 return null;
             }
-            
+
             return [$key];
         }
     }
-    
+
     /**
      * コンフィグデータを返す
-     * 
+     *
      * @param   string  $key
-     * 
+     *
      * @return  mixed|null
      */
     public static function get(string $key){
         $nodes  = [self::$config];
         $keys   = explode(".", $key);
-        
+
         foreach($keys as $key){
             $newNodes   = [];
             foreach($nodes as $node){
                 $children   = self::getChildren($node, $key);
-                
+
                 if($children !== null){
                     foreach($children as $child){
                         $newNodes[] = $node[1][$child];
                     }
                 }
             }
-            
+
             $nodes  = $newNodes;
         }
-        
+
         if(empty($nodes)){
             return null;
         }else if(count($nodes) === 1){
@@ -96,180 +96,180 @@ abstract class Configure{
             return $return;
         }
     }
-    
+
     /**
      * コンフィグデータが存在するか確認する
-     * 
+     *
      * @param   string  $key
-     * 
+     *
      * @return  bool
      */
     public static function has(string $key){
         $node   = self::$config;
-        
+
         foreach(explode(".", $key) as $key){
             if(!isset($node[1][$key])){
                 return false;
             }
-            
+
             $node   = $node[1][$key];
         }
-        
+
         return array_key_exists(0, $node);
     }
-    
+
     /**
      * コンフィグデータを設定する
-     * 
+     *
      * @param   string  $key
      * @param   mixed   $val
      * @param   int $option
-     * 
+     *
      * @throws  Exception\ConfigureException
-     * 
+     *
      * @return  void
      */
     public static function set(string $key, $val, int $option = null){
         $node   = &self::$config;
         $_key   = $key;
-        
+
         foreach(explode(".", $key) as $key){
             if(!isset($node[1][$key])){
                 $node[1][$key]  = [1 => []];
             }
-            
+
             $node = &$node[1][$key];
         }
-        
+
         if(array_key_exists(0, $node) && array_key_exists(2, $node)){
             if(!($node[2] & self::ALLOW_OVERWRITE)){
                 throw Exception\ConfigureException::overwrite($_key);
             }
         }
-        
+
         $node[0]    = $val;
         $node[2]    = $option ?? (array_key_exists(2, $node) ? $node[2] : 0);
     }
-    
+
     /**
      * DIコンテナを取得する
-     * 
+     *
      * @return  ContainerInterface
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getContainer(){
         $key    = "app.container";
         $c      = self::get($key);
-        
+
         if(!($c instanceof ContainerInterface)){
             throw Exception\ConfigureException::unexpectedValue(
-                "app.container", ContainerInterface::class
+                $key, ContainerInterface::class
             );
         }
-        
+
         return $c;
     }
-    
+
     /**
      * DIコンテナを登録する
-     * 
+     *
      * @param   ContainerInterface  $container
-     * 
+     *
      * @return  void
      */
     public static function setConteiner(ContainerInterface $container){
         $key    = "app.container";
-        
+
         self::set($key, $container);
     }
-    
+
     /**
      * イベントを取得する
-     * 
+     *
      * @param   string  $key
-     * 
+     *
      * @return  EventManagerInterface|null
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getEvent(string $key){
         $key    = "app.event.{$key}";
         if(self::has($key)){
             $event  = self::get($key);
-            
+
             if(!is_callable($event)){
                 throw Exception\ConfigureException::unexpectedValue(
                     $key, "callable"
                 );
             }
-            
+
             return $event;
         }
-        
+
         return null;
     }
-    
+
     /**
      * イベントを登録する
-     * 
+     *
      * @param   string  $key
      * @param   callable    $event
-     * 
+     *
      * @return  void
      */
     public static function setEvent(string $key, callable $event){
         $key    = "app.event.{$key}";
-        
+
         self::set($key, $event);
     }
-    
+
     /**
      * デバッグモードが有効かを返す
-     * 
+     *
      * 引数にboolを指定するとその値で上書きされる
-     * 
+     *
      * @param   bool    $debug
      *
      * @return  bool
      */
     public static function isDebug(bool $debug = null){
         $key    = "app.debug";
-        
+
         if($debug !== null){
             self::set($key, $debug);
         }else{
             $debug  = self::get($key);
-            
+
             if(!is_bool($debug)){
                 throw Exception\ConfigureException::unexpectedValue($key, "bool");
             }
         }
-        
+
         return $debug;
     }
-    
+
     /**
      * ルートディレクトリのパスを返す
-     * 
+     *
      * @return  string
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getRootPath(){
         $key    = "app.path.root";
-        
+
         if(self::has($key)){
             $path   = self::get($key);
         }else{
             $path   = isset($_SERVER["DOCUMENT_ROOT"])
                 ? realpath($_SERVER["DOCUMENT_ROOT"] . "/..") : null;
         }
-        
+
         if(!is_string($path)){
             throw Exception\ConfigureException::unexpectedValue($key, "string");
         }
-        
+
         return $path;
     }
 
@@ -277,22 +277,22 @@ abstract class Configure{
      * コンフィグディレクトリのパスを返す
      *
      * @return  string
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getConfigPath(){
         $key    = "app.path.config";
-        
+
         if(self::has($key)){
             $path   = self::get($key);
-            
+
             if(!is_string($path)){
                 throw Exception\ConfigureException::unexpectedValue($key, "string");
             }
         }else{
             $path   = self::getRootPath() . DS . "var" . DS . "config";
         }
-        
+
         return $path;
     }
 
@@ -300,22 +300,22 @@ abstract class Configure{
      * キャッシュディレクトリのパスを返す
      *
      * @return  string
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getCachePath(){
         $key    = "app.path.cache";
-        
+
         if(self::has($key)){
             $path   = self::get($key);
-            
+
             if(!is_string($path)){
                 throw Exception\ConfigureException::unexpectedValue($key, "string");
             }
         }else{
             $path   = self::getRootPath() . DS . "var" . DS . "cache";
         }
-        
+
         return $path;
     }
 
@@ -323,22 +323,22 @@ abstract class Configure{
      * ログディレクトリのパスを返す
      *
      * @return  string
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getLogPath(){
         $key    = "app.path.log";
-        
+
         if(self::has($key)){
             $path   = self::get($key);
-            
+
             if(!is_string($path)){
                 throw Exception\ConfigureException::unexpectedValue($key, "string");
             }
         }else{
             $path   = self::getRootPath() . DS . "var" . DS . "logs";
         }
-        
+
         return $path;
     }
 
@@ -346,22 +346,22 @@ abstract class Configure{
      * テンポラリディレクトリのパスを返す
      *
      * @return  string
-     * 
+     *
      * @throws  Exception\ConfigureException
      */
     public static function getTempPath(){
         $key    = "app.path.temp";
-        
+
         if(self::has($key)){
             $path   = self::get($key);
-            
+
             if(!is_string($path)){
                 throw Exception\ConfigureException::unexpectedValue($key, "string");
             }
         }else{
             $path   = sys_get_temp_dir();
         }
-        
+
         return $path;
     }
 }
