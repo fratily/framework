1610616c54fc6fe5f71b4e4ec7c896f3dcaf4fec
kento-oka <kento.oka@kentoka.com>
2018-04-07 18:06:56 +0900

    composerのクラスローダーを取得する関数を追加(使うかはわからない)

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * FratilyPHP
- * 
+ *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE
  * Redistributions of files must retain the above copyright notice.
- * 
+ *
  * @author      Kento Oka <kento.oka@kentoka.com>
  * @copyright   (c) Kento Oka
  * @license     MIT
@@ -17,4 +17,33 @@ if(!defined("DS")){
      * Alias of DIRECTORY_SEPARATOR.
      */
     define("DS", DIRECTORY_SEPARATOR);
+}
+
+if(!function_exists("getComposerClassLoader")){
+    /**
+     * composerのクラスローダーを取得する
+     *
+     * @return  \Composer\Autoload\ClassLoader|false
+     */
+    function getComposerClassLoader(){
+        static $loader;
+
+        if($loader === null){
+            $loader = false;
+
+            foreach(get_declared_classes() as $class){
+                if(substr($class, 0, 24) === "ComposerAutoloaderInited"
+                    && method_exists($class, "getLoader")
+                ){
+                    $tmp    = $class::getLoader();
+
+                    if($tmp instanceof \Composer\Autoload\ClassLoader){
+                        $loader = $tmp;
+                    }
+                }
+            }
+        }
+
+        return $loader;
+    }
 }
