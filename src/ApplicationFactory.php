009077c7cf773cc9a8446db925fcd7c83bf41e40
kento-oka <kento.oka@kentoka.com>
2018-06-05 12:41:46 +0900

    引数間違えていた

--- src/ApplicationFactory.php
+++ src/ApplicationFactory.php
@@ -64,7 +64,7 @@ class ApplicationFactory{
                 ],
                 $containerConfig,
                 [
-                    new Container\CoreConfig(),
+                    new Container\CoreConfig($debug),
                 ]
             );
 
