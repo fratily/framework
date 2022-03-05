732bb365a86177b257d889494cba50e6f4fb7932
kento-oka <kento-oka@kentoka.com>
2022-03-06 02:26:01 +0900

    コントローラーコールバックを直接設定するように 間に別のミドルウェアを入れないように

--- src/Routing/RoutingMiddleware.php
+++ src/Routing/RoutingMiddleware.php
@@ -33,7 +33,7 @@ class RoutingMiddleware implements MiddlewareInterface
             }
         } else {
             $request = $request
-                ->withAttribute(RequestAttribute::ROUTING_MATCH_ROUTE_CALLBACK, $match['controller'])
+                ->withAttribute(RequestAttribute::CONTROLLER_CALLBACK, $match['controller'])
                 ->withAttribute(RequestAttribute::ROUTING_MATCH_ROUTE_PARAMS, $match['params']);
         }
 
